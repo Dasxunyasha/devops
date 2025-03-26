ff437bf0f7e89ebfd49035b16e188c46dfa23cd4
1KarinaV <red@itmo.ru>
2025-03-26 23:16:24 +0300

    update cors policy

--- backend/src/main/java/itmo/devops/DevopsApplication.java
+++ backend/src/main/java/itmo/devops/DevopsApplication.java
@@ -2,11 +2,49 @@ package itmo.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
+import java.util.List;
+
+@EnableWebSecurity
 @SpringBootApplication
 public class DevopsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DevopsApplication.class, args);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public CorsConfiguration corsConfiguration() {
+        var configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        return configuration;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http,
+                                           CorsConfiguration configuration) throws Exception {
+        return http
+                .httpBasic().disable()
+                .csrf().disable()
+                .authorizeHttpRequests(
+                        auth -> auth.anyRequest().permitAll()
+                )
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .logout()
+                .and()
+                .exceptionHandling()
+                .and()
+                .cors().configurationSource(request -> configuration)
+                .and().build();
     }
 }
